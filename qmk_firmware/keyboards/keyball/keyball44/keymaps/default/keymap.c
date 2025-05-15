a190bc33e98c90279383a6a5cc1550cb7425dac9
sugimoto6654 <sugimoto.hinata.q2@dc.tohoku.ac.jp>
2025-05-15 16:30:53 +0900

    correct typo

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -102,9 +102,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ---------+--------+---------+---------+--------+--------+                     +---------+--------+--------+--------+---------+-----------
         KBC_RST , SCRL_TO, SCRL_DVD, SCRL_DVI, XXXXXXX, XXXXXXX,                       RGB_MOD , RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN,
     // ---------+--------+---------+---------+--------+--------+                     +---------+--------+--------+--------+---------+-----------
-        KBC_SAVE, AML_TO , AML_D50 , AML_I50 , XXXXXXX, QK_BOOT,                       RGB_RMOD, XXXXXX , XXXXXX , XXXXXX , XXXXXXX , XXXXXXX ,
+        KBC_SAVE, AML_TO , AML_D50 , AML_I50 , XXXXXXX, QK_BOOT,                       RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX ,
     // ---------+--------+---------+---------+--------+--------+---------     -------+---------+--------+--------+--------+---------+-----------
-                            _______, _______ , _______, _______, _______,     _______, _______ ,         XXXXXXX, XXXXXXX, RGB_TOG
+                            _______, _______ , _______, _______, _______,     _______, _______ ,          XXXXXXX, XXXXXXX, RGB_TOG
     //                   +---------+---------+--------+--------+---------     -------+---------+        +--------+--------+----------
   ),
 };
